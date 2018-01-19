d268594b9e0dd994ae0d0d316f6da386f0a46c77
Zephilinox <zephilinox@hotmail.co.uk>
2018-01-19 19:14:41 +0000

    .

--- Source/BirdmanTheGame.cpp
+++ Source/BirdmanTheGame.cpp
@@ -37,7 +37,6 @@ bool BirdmanTheGame::init()
 	key_handler_id = inputs->addCallbackFnc(ASGE::EventType::E_KEY, &BirdmanTheGame::keyHandler, this);
 
 	game_data->getFontManager()->addFont("../../Resources/Fonts/Comic.ttf", "Default");
-	game_data->getFontManager()->addFont("../../Resources/Fonts/Comic.ttf", "Default");
 	game_data->getStateManager()->push<MenuState>();
 
 	return true;
